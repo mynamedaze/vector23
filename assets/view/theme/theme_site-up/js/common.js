8de51a3e901dd2f6b39e9aa5f799bddfbd24642d
Murat Kudainetov <murat.kudainetov@gmail.com>
2020-12-10 17:04:07 +0300

    добавил кнопки соцсетей в выпадающее меню в шапке сайта

--- assets/view/theme/theme_site-up/js/common.js
+++ assets/view/theme/theme_site-up/js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('.page-header__burger-btn').click(function () {
     $('.page-header__burger-btn').toggleClass('active');
-    $('.main-nav__list').toggleClass('active');
+    $('.main-nav__container').toggleClass('active');
   });
 });
